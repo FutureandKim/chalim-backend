1084f36d03a853d4fcad5bc6e3c3170a93df971c
Yeaeun411 <yeaeun411@naver.com>
2023-11-21 13:57:39 +0900

    Your commit message here

---
+++ src/test/java/com/chalim/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalim.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
