14fd209c87d1ab4fb84430485ef1c33d73ed1d5b
MiraeKim <hufs202000512@gmail.com>
2023-11-30 11:07:42 +0900

    docs: response 수정

--- src/main/java/com/chalim/backend/dto/NameList.java
+++ src/main/java/com/chalim/backend/dto/NameList.java
@@ -4,22 +4,10 @@ import com.chalim.backend.dto.kakaoapi.Document;
 import java.util.List;
 
 public class NameList {
-    private double latitude;
-    private double longitude;
     private List<Document> documents;
 
-    public NameList(double latitude, double longitude, List<Document> documents) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+    public NameList(List<Document> documents) {
         this.documents = documents;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
     }
 
     public List<Document> getDocuments() {
@@ -29,9 +17,7 @@ public class NameList {
     @Override
     public String toString() {
         return "NameList{" +
-                "latitude=" + latitude +
-                ", longitude=" + longitude +
-                ", documents=" + documents +
+                "documents=" + documents +
                 '}';
     }
 }
