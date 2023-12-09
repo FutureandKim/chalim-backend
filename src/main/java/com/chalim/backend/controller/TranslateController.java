584c3ee285ead88b287db95517d45b2abeaacc68
MiraeKim <hufs202000512@gmail.com>
2023-12-10 02:50:50 +0900

    feat: 워드클라우드 kor

--- src/main/java/com/chalim/backend/controller/TranslateController.java
+++ src/main/java/com/chalim/backend/controller/TranslateController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/translate")
 public class TranslateController {
 
+
     private final TranslateService translateService;
 
     @Autowired
