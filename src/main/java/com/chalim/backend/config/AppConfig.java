5f0a41584f544ba38879b224a77bcc1edda35139
MiraeKim <hufs202000512@gmail.com>
2023-12-10 02:49:07 +0900

    feat: 워드클라우드 kor

--- src/main/java/com/chalim/backend/config/AppConfig.java
+++ src/main/java/com/chalim/backend/config/AppConfig.java
@@ -1,8 +1,5 @@
 package com.chalim.backend.config;
 
-import com.chalim.backend.controller.ReviewController;
-import com.chalim.backend.controller.WordCloudController;
-import com.chalim.backend.service.ReviewService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
